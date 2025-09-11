4cdd6ca4440db769ddfb3bcc3dd9464a8ab74bfa
Jeremiah Nofrada <jnnofrada@gmail.com>
2025-09-11 17:51:40 -0400

    added getComputerChoice function

--- rps.js
+++ rps.js
@@ -1,1 +1,14 @@
 console.log("Hello World!");
+
+function getComputerChoice() {
+    const randomChoice = Math.random();
+    if (randomChoice <= .33) {
+        return "rock";
+    } else if (randomChoice >= .66){
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
